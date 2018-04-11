aabe8109dcfcb745dcade4a332477780d3d9f564
Fitz <f18846188605@gmail.com>
2018-04-11 23:47:52 +0800

    working on abstract class edge

--- src/edge/Edge.java
+++ src/edge/Edge.java
@@ -1,5 +1,23 @@
 package edge;
 
-public class Edge {
+import vertex.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
+abstract public class Edge {
+    final List<Vertex> vertices = new LinkedList<>();
+    private final String label;
+    double weight;
+
+    Edge(String label, double weight) {
+        this.label = label;
+        this.weight = weight;
+    }
+
+    abstract public boolean addVertices(List<Vertex> vertices);
+
+    boolean containVertex(Vertex v) {
+        return vertices.contains(v);
+    }
 }
