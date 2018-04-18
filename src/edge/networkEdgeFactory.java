765f9b96b2d8be609eb52e1ed9c13962f13f5005
Fitz <f18846188605@gmail.com>
2018-04-18 23:52:40 +0800

    完善了app3的建图

--- src/edge/networkEdgeFactory.java
+++ src/edge/networkEdgeFactory.java
@@ -7,6 +7,12 @@ import java.util.List;
 class networkEdgeFactory {
     static Edge createEdge(String label, List<Vertex> vertices, double weight) {
         Edge networkEdge = new NetworkConnection(label, weight);
+        if (vertices.get(0).equals(vertices.get(1))) {
+            throw new UnsupportedOperationException("One networkConnection edge can't be a loop");
+        }
+        if (vertices.get(0).getClass().getName().equals(vertices.get(1).getClass().getName())) {
+            throw new UnsupportedOperationException("One networkConnection edge can't have the same type vertices");
+        }
         networkEdge.addVertices(vertices);
         return networkEdge;
     }
