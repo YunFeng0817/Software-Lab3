43f77cd83923d0c57bfb2ad9c100ce4799998c27
Fitz <f18846188605@gmail.com>
2018-04-19 12:23:16 +0800

    完成了app2的单元测试框架搭建

--- src/application/SocialNetworkApp.java
+++ src/application/SocialNetworkApp.java
@@ -1,4 +1,14 @@
 package application;
 
+import graph.Graph;
+import graph.GraphFactory;
+
+import java.io.IOException;
+
 public class SocialNetworkApp {
+    Graph socialNetworkGraph;
+
+    SocialNetworkApp(String filePath) throws IOException {
+        socialNetworkGraph = GraphFactory.createGraph(filePath);
+    }
 }
