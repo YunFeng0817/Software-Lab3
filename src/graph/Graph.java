42f0b0ee8809beba3746b8a344a0fac4c06d7a27
Fitz <f18846188605@gmail.com>
2018-04-20 18:17:20 +0800

    add some comments to the Interface Graph

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -10,71 +10,70 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * A mutable weighted graph with labeled vertices.
- * Vertices have distinct labels of an immutable type {@code Vertex} when compared
- * using the {@link Object#equals(Object) equals} method.
- * Edgedges have multiple types and have a non-negative weight of type {@code double}.
- * <p>
- * <p>PS2 instructions: this is a required ADT interface.
- * You MUST NOT change the specifications or add additional methods.
- */
 public interface Graph {
 
     /**
-     * Add a vertex to this graph.
+     * 向图中添加一个Vertex对象
      *
-     * @param vertex label for the new vertex
-     * @return true if this graph did not already include a vertex with the
-     * given label; otherwise false (and this graph is not modified)
+     * @param vertex Vertex对象,表示一个点
+     * @return true: 向图中成功的添加了这个点 false: 图中已经包含了这个点
      */
     public boolean addVertex(Vertex vertex);
 
     /**
-     * Remove a vertex from this graph; any edges to or from the vertex are
-     * also removed.
+     * 从图中移除这个点，任何包含这个点的边都会同时被删除
      *
-     * @param vertex label of the vertex to remove
-     * @return true if this graph included a vertex with the given label;
-     * otherwise false (and this graph is not modified)
+     * @param vertex 需要移除的点对象
+     * @return true ：移除成功 false: 图中不包含这个点，删除失败
      */
     public boolean removeVertex(Vertex vertex);
 
     /**
-     * Get all the vertices in this graph.
+     * 得到图中的所有点对象
      *
-     * @return the set of labels of vertices in this graph
+     * @return 图中所有点的一个集合
      */
     public Set<Vertex> vertices();
 
     /**
-     * Get the source vertices with directed edges to a target vertex and the
-     * weights of those edges.
+     * 得到以传入点为终点的所有边的起点，以及所有边的权重值
      *
-     * @param target a label
-     * @return a map where the key set is the set of labels of vertices such
-     * that this graph includes an edge from that vertex to target, and
-     * the value for each key is the (nonzero) weight of the edge from
-     * the key to target
+     * @param target 一个点的对象
+     * @return 一个点和一个权重值的数组形成的map, 点是边的起点，边的终点就是传入的点
      */
     public Map<Vertex, List<Double>> sources(Vertex target);
 
     /**
      * Get the target vertices with directed edges from a source vertex and the
      * weights of those edges.
+     * 得到以传入点为起点的所有边的终点，以及所有边的权重值
      *
-     * @param source a label
-     * @return a map where the key set is the set of labels of vertices such
-     * that this graph includes an edge from source to that vertex, and
-     * the value for each key is the (nonzero) weight of the edge from
-     * source to the key
+     * @param source 一个点的对象
+     * @return 一个点和一个权重值的数组形成的map, 点是边的终点，边的起点就是传入的点
      */
     public Map<Vertex, List<Double>> targets(Vertex source);
 
+    /**
+     * 向图中添加一条边
+     *
+     * @param edge 一个边的对象，表示一条边
+     * @return true: 图中成功添加了这条边 false: 图中已经有了这条边，添加失败
+     */
     public boolean addEdge(Edge edge);
 
+    /**
+     * 从图中移除指定的边对象
+     *
+     * @param edge 一个边的对象，表示一条边
+     * @return true: 成功的从图中删除了这条边 false: 图中没有传入的边对象，删除失败
+     */
     public boolean removeEdge(Edge edge);
 
+    /**
+     * 得到图中的所有边对象
+     *
+     * @return 图中所有边对象的集合
+     */
     public Set<Edge> edges();
 
 }
