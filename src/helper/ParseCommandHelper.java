4f18ec54d56540d193052c6a6c9c4c0c87e30dba
Fitz <f18846188605@gmail.com>
2018-04-28 23:11:16 +0800

    work on parse command

--- src/helper/ParseCommandHelper.java
+++ src/helper/ParseCommandHelper.java
@@ -1,4 +1,53 @@
 package helper;
 
 public class ParseCommandHelper {
+    public static void main(String[] args) {
+        switch (args[0]) {
+            case "vertex":
+                break;
+            case "edge":
+                break;
+            case "hyperedge":
+                break;
+            default:
+                throw new UnsupportedOperationException("usage");
+        }
+    }
+
+    static class vertex {
+        static void vertex(String[] args) {
+
+        }
+
+        static void add() {
+
+        }
+
+        static void delete() {
+
+        }
+    }
+
+    static class edge {
+        static void edge(String[] args) {
+
+        }
+
+        static void add() {
+
+        }
+
+        static void delete() {
+        }
+    }
+
+    static class hyperedge {
+        static void hyperedge(String[] args) {
+
+        }
+
+        static void add() {
+
+        }
+    }
 }
