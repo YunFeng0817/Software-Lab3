be6cc89a83f49de5565c68c247b35184f0b85002
Fitz <f18846188605@gmail.com>
2018-05-03 18:01:45 +0800

    完成了图的可视化

--- src/helper/GraphVisualizationHelper.java
+++ src/helper/GraphVisualizationHelper.java
@@ -7,10 +7,13 @@ import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.AbstractGraph;
 import edu.uci.ics.jung.graph.UndirectedOrderedSparseMultigraph;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer;
 import factory.graph.GraphFactory;
 import graph.Graph;
 import graph.GraphPoet;
 import graph.SocialNetwork;
+import org.apache.commons.collections15.Transformer;
 import vertex.Vertex;
 
 import javax.swing.*;
@@ -23,8 +26,21 @@ public class GraphVisualizationHelper {
         Layout<Vertex, Edge> layout = new CircleLayout(transferGraph(g));
         layout.setSize(new Dimension(300, 300)); // sets the initial size of the space     // The BasicVisualizationServer<V,E> is parameterized by the edge types
         BasicVisualizationServer<Vertex, Edge> vv = new BasicVisualizationServer<Vertex, Edge>(layout);
-        vv.setPreferredSize(new Dimension(350, 350)); //Sets the viewing area size
+        vv.setPreferredSize(new Dimension(600, 600)); //Sets the viewing area size
         JFrame frame = new JFrame("Simple Graph View");
+
+        Transformer<Vertex, Paint> vertexPaint = vertex -> Color.BLUE;
+        // Set up a new stroke Transformer for the edges
+        float dash[] = {10.0f};
+        final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
+        Transformer<Edge, Stroke> edgeStrokeTransformer = s -> edgeStroke;
+        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+        vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(vv);
         frame.pack();
@@ -38,6 +54,9 @@ public class GraphVisualizationHelper {
         topologyNetwork = GraphFactory.createGraph("./test/graph/data/GraphTopology.txt");
         movie = GraphFactory.createGraph("./test/graph/data/GraphMovie.txt");
         GraphVisualizationHelper.visualize(poet);
+        GraphVisualizationHelper.visualize(socialNetwork);
+        GraphVisualizationHelper.visualize(topologyNetwork);
+        GraphVisualizationHelper.visualize(movie);
     }
 
 
