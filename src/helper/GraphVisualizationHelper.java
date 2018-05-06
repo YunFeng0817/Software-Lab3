11c5faabde3b0ca7aec21dc8491a28562d3ac267
Fitz <f18846188605@gmail.com>
2018-05-06 21:37:03 +0800

    向command类添加了指令的使用方法

--- src/helper/GraphVisualizationHelper.java
+++ src/helper/GraphVisualizationHelper.java
@@ -22,6 +22,39 @@ import java.util.ArrayList;
 import java.util.*;
 import java.util.List;
 
+/**
+ * 点的命令
+ * vertex --add label type  向图中添加名为label，类型为type的点
+ * vertex --delete regex 删除所有label符合regex规则的点
+ * vertex --update label
+ *              label=    改变点的label值
+ *              argument=   改变点的argument值，即点的属性
+ * vertex --show
+ *              .eccentricity 求点的eccentricity值
+ *              degree 求点的degree值
+ *              indegree 求点的indegree值
+ *              outdegree 求点的outdegree值
+ *              closenessCentrality 求点的closenessCentrality值
+ *              betweennessCentrality 求点的 betweennessCentrality值
+ *
+ * 边的命令
+ * edge --add label type [weighted=Y|N] [weight] [directed=Y|N] v1, v2
+ * edge --delete regex 删除所有label符合regex的边
+ *
+ * 超边的命令
+ * hyperedge --add
+ *                  label type vertex1,    ..., vertexn 向图中添加一个新的超边
+ *                  label vertex1,    ..., vertexn 向label 为 label的超边添加点
+ * hyperedge --delete label regex 删除超边中label符合regex规则的点
+ *
+ * 图的命令
+ * graph --show
+ *              degreeCentrality 输出图的degreeCentrality值
+ *              radius 输出图的radius值
+ *              diameter 输出图的diameter值
+ *              visiable 将图的可视化结果输出
+ *              distance="vertex1""vertex2" 输出vertex1 与 vertex2 在图中的 distance值
+ */
 public class GraphVisualizationHelper {
     public static void visualize(Graph g) {
         CircleLayout<Vertex, Edge> layout = new CircleLayout<>(transferGraph(g));
