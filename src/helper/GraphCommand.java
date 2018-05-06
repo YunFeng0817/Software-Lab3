fca1bfb6941a0f47ed872a9fda670495937afbe3
Fitz <f18846188605@gmail.com>
2018-05-06 17:11:57 +0800

    完成了图的基本信息的展示的命令行操作解析

--- src/helper/GraphCommand.java
+++ src/helper/GraphCommand.java
@@ -1,6 +1,12 @@
 package helper;
 
 import graph.Graph;
+import vertex.Vertex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GraphCommand extends Command {
     GraphCommand(Graph g) {
@@ -24,6 +30,31 @@ public class GraphCommand extends Command {
 
     @Override
     void show(String[] args) {
-
+        Matcher matcher;
+        StringBuilder OptionalCommand = new StringBuilder();
+        for (int i = 4; i < args.length; i++) {
+            OptionalCommand.append(args[i]);
+        }
+        List<Pattern> Rules = new ArrayList<>();
+        Rules.add(Pattern.compile("degreeCentrality"));
+        Rules.add(Pattern.compile("radius"));
+        Rules.add(Pattern.compile("diameter"));
+        Rules.add(Pattern.compile("visiable"));
+        matcher = Rules.get(0).matcher(OptionalCommand);
+        if (matcher.find()) {
+            System.out.println("The degreeCentrality of the graph : " + GraphMetrics.degreeCentrality(graph));
+        }
+        matcher = Rules.get(1).matcher(OptionalCommand);
+        if (matcher.find()) {
+            System.out.println("The radius of the graph : " + GraphMetrics.radius(graph));
+        }
+        matcher = Rules.get(2).matcher(OptionalCommand);
+        if (matcher.find()) {
+            System.out.println("The diameter of the graph : " + GraphMetrics.diameter(graph));
+        }
+        matcher = Rules.get(3).matcher(OptionalCommand);
+        if (matcher.find()) {
+            GraphVisualizationHelper.visualize(graph);
+        }
     }
 }
